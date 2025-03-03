c22a7c02c636dd1020c15a0a8ae61bb7a5966607
TUNGNT <nguyenthanhtunnghutechsg@gmail.com>
2025-03-03 10:34:47 +0700

    CRUD

--- main.js
+++ main.js
@@ -1,94 +1,99 @@
-let keyScore1 = "Toan";
-let keyScore2 = "Tin";
-let Student = function(id,name,age,score1,score2){
-    this.id = id;
-    this.name = name;
-    this.age = age;
-    this.scores={
-        [keyScore1]: score1,
-        [keyScore2]: score2
-    }
-    this.getInfo = function(){
-        return id;
-    }
+let URL = "http://localhost:3000/posts";
+var global;
+LoadSync();
+function Load(){
+    fetch(URL).then(function(response){
+        return response.json();
+    }).then(function(data){
+        console.log(data);
+    }).catch(function(error){
+        console.log(error);
+    })
 }
-let students = [];
-students.push(new Student(4,"Tung",17,9,9));
-students.push(new Student(3,"Toan",18,2,2));
-students.push(new Student(2,"Tien",18,2,9));
-students.push(new Student(1,"Tuan",18,3,4));
-// let result = [];
-// for (const student of students) {
-//     if(student.scores[keyScore1]+student.scores[keyScore2]>=16){
-//         result.push("Gioi");
-//     }else{
-//         if(student.scores[keyScore1]+student.scores[keyScore2]>=13){
-//             result.push("Kha");
-//         }else{
-//             if (student.scores[keyScore1]+student.scores[keyScore2]>=8) {
-//                 result.push("TB");
-//             } else {
-//                 result.push("Kem");
-//             }
-//         }
-//     }
-// }
-let result = students.map(
-    function(student){
-    if(student.scores[keyScore1]+student.scores[keyScore2]>=16){
-        return "Gioi";
-    }else{
-        if(student.scores[keyScore1]+student.scores[keyScore2]>=13){
-            return "Kha";
-        }else{
-            if (student.scores[keyScore1]+student.scores[keyScore2]>=8) {
-                return "TB";
-            } else {
-                return "Kem";
-            }
+async function LoadSync(){
+    try {
+        let response = await fetch(URL);
+        let posts = await response.json();
+        posts = posts.filter(p=>!p.isDeleted);
+        global = posts;
+        let body = document.getElementById("body");
+        body.innerHTML="";
+        for (const post of posts) {
+            body.innerHTML+=ConvertFromObjToHTML(post);
         }
+    } catch (error) {
+        console.log(error);
     }
-    }
-);
-
-// let sum = 0;
-// for (const student of students) {
-//     sum+=student.age;
-// }
-
-let max = students.reduce(function(max,student){
-    return max<student.age?student.age:max;
-},students[0].age)
-let sum = students.reduce(function(sum,student){
-    return sum+=student.age;
-},0)
-
-// let Duoi18 = [];
-// for (const student of students) {
-//     if(student.age<18){
-//         Duoi18.push(student);
-//     }
-// }
-let Duoi18 = students.filter(function(student){
-    return student.age<18;
-})
-
-let checkDup = students.some(
-    function(student){
-        return student.scores.score1+student.scores.score1<8;
-    }
-)
-let checkQUaMon = students.every(
-    function(student){
-        return student.scores.score1+student.scores.score1>8;
-    }
-)
-
-students.sort(Compare)
-
-function Compare(student1,student2){
-    if(student1.age==student2.age){
-        return student1.id-student2.id;
-    }
-    return student1.age-student2.age;
 }
+function ConvertFromObjToHTML(post){
+    let string = `<tr>`;
+    string +=`<td>${post.id}</td>`
+    string +=`<td>${post.title}</td>`
+    string +=`<td>${post.views}</td>`
+    string +=`<td><button onclick="Delete(${post.id});return false">Delete </button></td>`
+    string += `</tr>`
+    return string;
+}
+
+function CheckExist(id){
+    return global.find(function(p){
+        return p.id==id
+    })
+}
+function getMax(){
+    let ids = global.map(p=>Number.parseInt(p.id)); 
+    return Math.max(...ids);
+}
+
+function Save(){
+    let id = document.getElementById("id").value;
+    if(id.length==0||isNaN(id)){
+        id = (getMax()+1)+"";
+    }
+    let Obj = {
+        id:id,
+        title:document.getElementById("title").value,
+        views:document.getElementById("views").value
+    }
+    if(CheckExist(id)){
+        fetch(URL+"/"+id,{
+            method:'PUT',
+            body:JSON.stringify(Obj),
+            headers:{
+                "Content-Type":"application/json",
+            }
+        }).then(function(){
+            LoadSync();
+        })
+    }else{
+        fetch(URL,{
+            method:'POST',
+            body:JSON.stringify(Obj),
+            headers:{
+                "Content-Type":"application/json",
+            }
+        }).then(function(){
+            LoadSync();
+        })
+    }
+}
+
+function Delete(id){
+    let post = CheckExist(id);
+    if(post){
+        post.isDeleted = true;
+        fetch(URL+"/"+id,{
+            method:'PUT',
+            body:JSON.stringify(post),
+            headers:{
+                "Content-Type":"application/json",
+            }
+        }).then(function(){
+            LoadSync();
+        })
+    }else{
+        //
+    }
+    
+}
+
